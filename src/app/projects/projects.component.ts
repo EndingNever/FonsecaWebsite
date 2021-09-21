030f98bc1b30dcf143e62249838634e21e83f767
Fabian Villasenor <exionite@gmail.com>
2021-09-21 15:44:30 -0700

    mobile

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -25,51 +25,24 @@ export class ProjectsComponent implements OnInit {
     switch(this.hover) {
       case this.hover = 'hover1': {
         this.hover1 = true;
-        console.log(`hover = hover1, hover1 = ` + this.hover1);
         break;
       }
       case this.hover = 'hover2': {
         this.hover2 = true;
-        console.log(`hover = hover2, hover2 = ` + this.hover2);
         break;
       }
       case this.hover = 'hover3' : {
         this.hover3 = true;
-        console.log(`hover = hover3, hover3 = ` + this.hover3);
         break; 
       }
       case this.hover = 'hover4' : {
         this.hover4 = true;
-        console.log(`hover = hover4, hover4 = ` + this.hover4);
         break;
+        // console.log(`hover = hover4, hover4 = ` + this.hover4);
       }
     }
   }
 
-  mouseLeave(num:number) {
-    // switch(this.hover) {
-    //   case this.hover = 'hover1': {
-    //     this.hover1 = false;
-    //     console.log(`hover = hover1, hover1 = ` + this.hover1);
-    //     break;
-    //   }
-    //   case this.hover = 'hover2': {
-    //     this.hover2 = false;
-    //     console.log(`hover = hover2, hover2 = ` + this.hover2);
-    //     break;
-    //   }
-    //   case this.hover = 'hover3' : {
-    //     this.hover3 = false;
-    //     console.log(`hover = hover3, hover3 = ` + this.hover3);
-    //     break; 
-    //   }
-    //   case this.hover = 'hover4' : {
-    //     this.hover4 = false;
-    //     console.log(`hover = hover4, hover4 = ` + this.hover4);
-    //     break;
-    //   }
-    // }
-  }
   
   ngOnInit(): void {
   }
