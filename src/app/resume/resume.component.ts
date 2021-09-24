b4dd4c7af1d8d0e0115a8109ba7508bbeab47db7
Fabian Villasenor <exionite@gmail.com>
2021-09-23 17:43:08 -0700

    mobile styling / fixed pdf

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, OnInit, Output, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent implements OnInit {
-
+export class ResumeComponent implements OnInit, AfterViewInit {
+  pdf:any
+  isLoading!:boolean
+  
   constructor() { }
 
   ngOnInit(): void {
+    const myPDF = document.getElementById("pdf")
+    this.pdf = myPDF
+    console.log(this.pdf.offsetHeight) 
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+    
+  }
+
+  checkIfLoaded() {
+    // window.addEventListener("load", function(event) {
+    //   console.log("All resources finished loading");
+    // })
+    // this.isLoading = false;
+    // document.getElementById("pdf");
   }
 
 }
